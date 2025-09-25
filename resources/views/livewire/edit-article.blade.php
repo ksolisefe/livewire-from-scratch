f643eefacc866f01381b9bdd5402d9714b47cf49
ksolis <ksolis@efe1963.com>
2025-09-25 14:43:04 -0400

    Livewire dirty functionality is pretty neat!

--- resources/views/livewire/edit-article.blade.php
+++ resources/views/livewire/edit-article.blade.php
@@ -2,7 +2,10 @@
     <h3 class="text-lg text-gray-200 mb-3">Edit Article</h3>
     <form wire:submit="save">
         <div class="mb-3">
-            <label class="block" for="article-title">Title</label>
+            <label wire:dirty.class="text-orange-400" wire:target="form.title" class="block" for="article-title">
+                Title
+                <span wire:dirty wire:target="form.title">*</span>
+            </label>
             <input 
                 type="text" 
                 class="p-2 w-full border rounded-md bg-gray-700 text-white"
@@ -13,7 +16,10 @@
             </div>
         </div>
         <div class="mb-3">
-            <label class="block" for="article-content">Content</label>
+            <label wire:dirty.class="text-orange-400" wire:target="form.content" class="block" for="article-content">
+                Content
+                <span wire:dirty wire:target="form.content">*</span>
+            </label>
             <textarea 
                 id="article-content"
                 class="p-2 w-full border rounded-md bg-gray-700 text-white"
@@ -24,18 +30,22 @@
             </div>
         </div>
         <div class="mb-4">
-            <label class="flex items-center">
+            <label wire:dirty.class="text-orange-400" wire:target="form.published" class="flex items-center">
                 <input type="checkbox" name="published"
                     class="mr-2"
                     wire:model.boolean="form.published"
                 >
                     Published
+                    <span wire:dirty wire:target="form.published">*</span>
             </label>
         </div>
         <hr class="border-b border-gray-600/20 my-4">
         <div class="mb-3">
             <div>
-                <div class="mb-2">Notification Options</div>
+                <div wire:dirty.class="text-orange-400" wire:target="form.notifications" class="mb-2">
+                    Notification Options
+                    <span wire:dirty wire:target="form.notifications">*</span>
+                </div>
                 <div class="mb-3">
                     <div>
                         <div class="flex gap-6">
@@ -78,8 +88,11 @@
         </div>
         <div class="mb-3">
             <button 
-                class="text-gray-200 p-2 bg-indigo-700 hover:bg-indigo-900 rounded-sm"
+                class="text-gray-200 p-2 bg-blue-700 rounded-sm disabled:opacity-75 disabled:bg-300"
                 type="submit"
+                wire:dirty.class="hover:bg-blue-900"
+                wire:dirty.attr.remove="disabled"
+                disabled
             >
                 Save
             </button>
